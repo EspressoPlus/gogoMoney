8d59cc2100a4d9b59f705c3a3bdf301d32bdc96a
Christopher <tophermartus@gmail.com>
2022-03-30 13:58:34 -0400

    Update 1

--- src/main/java/com/gogo/dao/UserDAOImpl.java
+++ src/main/java/com/gogo/dao/UserDAOImpl.java
@@ -1,8 +1,15 @@
 package com.gogo.dao;
 
+import java.util.List;
+
+
+import org.hibernate.query.Query;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.gogo.entity.User;
 
 @Repository
 public class UserDAOImpl implements UserDAO {
@@ -10,5 +17,22 @@ public class UserDAOImpl implements UserDAO {
 	@Autowired // tells hibernate to look for sessionFactory bean
 	private SessionFactory sessionFactory;
 	
+	//Queries the table and gets a list of users back
+	@Override
+	public List<User> getUsers() {
+		Session session = sessionFactory.getCurrentSession();
+		Query<User> query = session.createQuery("From User", User.class);
+		for(User user : query.getResultList()) {
+			System.out.println(user.getUname());
+		}
+		return query.getResultList();
+	}
+
+	//uses id to get the user directly from the sessionfactory
+	@Override
+	public User getUser(int id) {
+		Session session = sessionFactory.getCurrentSession();
+		return session.get(User.class, id);
+	}
 
 }
