c08481cb53ad3b5285a58ffce78190fc29642f88
nell2113 <77591003+nell2113@users.noreply.github.com>
2022-04-22 00:46:45 -0400

    Recurring vs. One time fee/income
    Corrected the populateFinance screen to reflect Anabelle's vision. Was a simply copy past for displayTransaction. But this shouldn't be merged until you all meet tomorrow. Had to get it done tonight because I feel horrible and am pretty sure I won't make it to the meeting.

--- src/main/java/com/gogo/service/MoneyServiceImpl.java
+++ src/main/java/com/gogo/service/MoneyServiceImpl.java
@@ -65,42 +65,57 @@ public class MoneyServiceImpl implements MoneyService {
 	public int getSavingsTime(Date date, User user)
 	{
 		int i = 0;
+		
+		//gets all income and outcome
 		List<Financial> income = moneyDAO.getIncomes(user.getUser_id());
 		List<Financial> outcome = moneyDAO.getOutcomes(user.getUser_id());
 		
+		//to put income and outcome totals in
 		Double inMonth = 0.00;
 		Double outMonth = 0.00;
 		
+		
+		//only adds recurring amounts
 		for(int j = 0; j < income.size();j++)
 		{
 			if(income.get(j).getTransaction_date().after(date))
 			{
-				inMonth = inMonth + income.get(j).getAmount();
+				if(income.get(j).getRecurring().equals("TRUE"))
+				{
+					inMonth = inMonth + income.get(j).getAmount();
+				}
 			}
 		}
 		
+		//only adds recurring amounts
 		for(int j = 0; j < outcome.size();j++)
 		{
 			if(outcome.get(j).getTransaction_date().after(date))
 			{
-				outMonth = outMonth + outcome.get(j).getAmount();
+				if(outcome.get(j).getRecurring().equals("TRUE"))
+				{
+					outMonth = outMonth + outcome.get(j).getAmount();
+				}
 			}
 		}
 		
+		//total surplus
 		Double total = inMonth - outMonth;
+		
+		//save goal amount
 		Double save = user.getAmount_to_save();
-		Double mon = 0.0;
+		
+		//if there is a left over amount, add an additional month
 		if(save%total>0.01)
 		{
 			Double t = (save/total) + 1;
 			i = t.intValue();
 		}
-		else
+		else //if saved/total = 0, then i is the exact amount of months it will take to save
 		{
 			Double t = (save/total);
 			i = t.intValue();
 		}
-			
 		
 		return i;
 	}
@@ -112,20 +127,67 @@ public class MoneyServiceImpl implements MoneyService {
 		Double income = 0.0;
 		Double outcome = 0.0;
 		
+		//gets all income and outcome
 		List<Financial> in = moneyDAO.getIncomes(user_id);
 		List<Financial> out = moneyDAO.getOutcomes(user_id);
 		
+		//only add the recurring amounts
 		for(int i = 0; i < in.size(); i++)
 		{
-			income+=in.get(i).getAmount();
+			if(in.get(i).getRecurring().equals("TRUE"))
+			{
+				income+=in.get(i).getAmount();
+			}
 		}
 		
+		//only add the recurring amounts
 		for(int i = 0; i < out.size(); i++)
 		{
-			outcome+=out.get(i).getAmount();
+			if(out.get(i).getRecurring().equals("TRUE"))
+			{
+				outcome+=out.get(i).getAmount();
+			}
 		}
 		
-		return income - outcome;
+		return income - outcome; //surplus amount
+	}
+	
+	//gets only the one time occurrence outcomes
+	@Override
+	@Transactional
+	public List<Financial> getOutcomesOneTime(int user_id)
+	{
+		List<Financial> out = moneyDAO.getOutcomes(user_id);
+		List<Financial> outcome = new ArrayList<Financial>();
+		//only add the recurring amounts
+		for(int i = 0; i < out.size(); i++)
+		{
+			if(out.get(i).getRecurring().equals("FALSE"))
+			{
+				outcome.add(out.get(i));
+			}
+		}
+		
+		return outcome;
+	}
+	
+	//gets only the one time occurrence incomes
+	@Override
+	@Transactional
+	public List<Financial> getIncomesOneTime(int user_id)
+	{
+		List<Financial> in = moneyDAO.getIncomes(user_id);
+		List<Financial> income = new ArrayList<Financial>();
+		//only add the recurring amounts
+		for(int i = 0; i < in.size(); i++)
+		{
+			if(in.get(i).getRecurring().equals("FALSE"))
+			{
+				income.add(in.get(i));
+			}
+		}
+		
+		return income;
 	}
 	
 	@Override
